da647a5dbcc4478dd3954cf41ac70de0e16e4590
Ilabarry <barryila20@gmail.com>
2025-10-04 11:31:37 +0100

    configuration pour le deploiement

--- Frontend-app/src/services/api.js
+++ Frontend-app/src/services/api.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", 
+  // baseURL: "http://127.0.0.1:8000/api", 
+  // headers: {
+  //   "Content-Type": "application/json",
+  //   Accept: "application/json",
+  // },
+
+  baseURL: "https://amused-magic.up.railway.app/api", // ton backend Railway
+  withCredentials: true, // 🔑 essentiel pour que le cookie Laravel soit envoyé
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
